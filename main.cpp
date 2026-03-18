a165cb90cdae68ac84162fcc599455f559f1fee4
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-17 17:22:51 -0700

    call function

--- main.cpp
+++ main.cpp
@@ -69,27 +69,27 @@ double calculateDiscountedStrikePrice(double strikePrice, double riskFreeRate, d
     return discountedStrikePrice;
 }
 
-// double calculateCall(double stockPrice, double strikePrice, double riskFreeRate, double volatility, double timeToExpiration)
-// {
-//     double s{stockPrice};
-//     double k{strikePrice};
-//     double r{riskFreeRate};
-//     double v{volatility};
-//     double t{timeToExpiration};
-//
-//     double d1{calculateD1(s, k, r, v ,t)};
-//     double nD1{calculateNormalCDF(d1)};
-//
-//     double d2{calculateD2(d1,s, k, r, v, t)};
-//     double nD2{calculateNormalCDF(d2)};
-//
-//     double discountedStrikePrice{calculateDiscountedStrikePrice(k, r,t)};
-//
-//     double callValue = s * nD1 - discountedStrikePrice * nD2;
-//
-//     return callValue;
-//
-// }
+double calculateCall(double stockPrice, double strikePrice, double riskFreeRate, double volatility, double timeToExpiration)
+{
+    double s{stockPrice};
+    double k{strikePrice};
+    double r{riskFreeRate};
+    double v{volatility};
+    double t{timeToExpiration};
+
+    double d1{calculateD1(s, k, r, v ,t)};
+    double nD1{calculateNormalCDF(d1)};
+
+    double d2{calculateD2(d1,s, k, r, v, t)};
+    double nD2{calculateNormalCDF(d2)};
+
+    double discountedStrikePrice{calculateDiscountedStrikePrice(k, r,t)};
+
+    double callValue = s * nD1 - discountedStrikePrice * nD2;
+
+    return callValue;
+
+}
 
 
 int main() {
